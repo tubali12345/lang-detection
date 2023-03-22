0579913547efd7a00d201b5582146f70c48f80b9
turib <turib@regens.com>
2023-03-22 12:05:24 +0100

    refactored print adn write metrics

--- eval/evaluate.py
+++ eval/evaluate.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 from config import Config
 from data.audio_preprocess import Aud2Mel, load_audio, split_audio
 from data.dataloader import ShortAudioDataSet, collate_fn
-from metrics import Metrics, get_lang_by_id
+from metrics import Metrics
 from models.resnet import ResNet50LangDetection
 
 
@@ -25,9 +25,9 @@ class EvaluateModel:
         self.aud_to_mel = Aud2Mel(Config.feature_dim, Config.sample_rate, 2048, 400, 160)
         self.device = device
 
-    def evaluate_short(self, test_data_path: str, no_samples_per_class: int = 10 ** 20) -> None:
-        t_pred = torch.Tensor([]).to(self.device)
-        t_labels = torch.Tensor([]).to(self.device)
+    def evaluate_short(self, test_data_path: str, no_samples_per_class: int = 10 ** 20) -> tuple:
+        pred = torch.Tensor([]).to(self.device)
+        labels = torch.Tensor([]).to(self.device)
 
         with torch.no_grad():
             for batch in tqdm(load_data(test_data_path, no_samples_per_class=no_samples_per_class), desc='Validating'):
@@ -37,12 +37,11 @@ class EvaluateModel:
                 out = self.model(mel.transpose(1, 2).to(self.device))
                 _, batch_predicted = out.max(1)
 
-                t_pred = torch.cat((t_pred, batch_predicted), 0)
-                t_labels = torch.cat((t_labels, y.to(self.device)), 0)
+                pred = torch.cat((pred, batch_predicted), dim=0)
+                labels = torch.cat((labels, y.to(self.device)), dim=0)
 
-        m2 = Metrics(t_pred, t_labels)
-        print(m2.accuracy())
-        print(m2.acc_by_lang())
+        metrics = Metrics(pred, labels)
+        return metrics.accuracy(), metrics.acc_by_lang()
 
     def evaluate_long(self,
                       test_data_path,
@@ -63,15 +62,13 @@ class EvaluateModel:
                         (predicted, torch.Tensor([Config.class_dict[predicted_class]]).to(self.device)), dim=0)
                     labels = torch.cat((labels, torch.Tensor([Config.class_dict[audio_class]]).to(self.device)), dim=0)
 
-            m = Metrics(predicted, labels)
-            print(m.acc_by_lang())
-            print(m.accuracy())
-            return m.accuracy(), m.acc_by_lang()
+            metrics = Metrics(predicted, labels)
+            return metrics.accuracy(), metrics.acc_by_lang()
 
     def predict_from_file(self, audio_path: str, batch_audio_chunk: int) -> str or None:
         audio = load_audio(audio_path)
         try:
-            audio_chunks = torch.cat([torch.unsqueeze(torch.FloatTensor(aud), 0) for aud in split_audio(audio)])
+            audio_chunks = torch.cat([torch.unsqueeze(torch.FloatTensor(aud), dim=0) for aud in split_audio(audio)])
         except Exception as e:
             print(audio_path, e)
             return
@@ -83,20 +80,37 @@ class EvaluateModel:
                 mel = self.aud_to_mel(audio_chunks[i:i + batch_audio_chunk])
                 out = self.model(mel.transpose(1, 2).to(self.device))
                 _, batch_predicted = out.max(1)
-                predicted = torch.cat((predicted, batch_predicted), 0)
+                predicted = torch.cat((predicted, batch_predicted), dim=0)
 
         return get_lang_by_id(int(np.argmax(np.bincount(list(map(int, predicted))))))
 
 
 def load_data(data_path: str,
-              no_samples_per_class,
+              no_samples_per_class: int,
               batch_size: int = 64,
               prefetch_factor: int = 4,
               num_workers: int = 1,
               ) -> DataLoader:
     return DataLoader(ShortAudioDataSet(data_path, random_sample=False, no_samples_per_class=no_samples_per_class),
-                      batch_size=batch_size, shuffle=False,
-                      collate_fn=collate_fn, pin_memory=False, num_workers=num_workers, prefetch_factor=prefetch_factor)
+                      batch_size=batch_size, shuffle=False, collate_fn=collate_fn,
+                      pin_memory=False, num_workers=num_workers, prefetch_factor=prefetch_factor)
+
+
+def print_and_write_metrics(accuracy: float,
+                            acc_by_lang: float,
+                            model_name: str,
+                            file_path: str = f'/home/turib/lang_detection/eval/scores_{date.today()}.txt'):
+    print(f'Model: {model_name} \n'
+          f'Accuracy: {accuracy} \n'
+          f'Accuracy by language: {acc_by_lang}')
+    Path(file_path).open('a').write(f'{model_name} \n'
+                                    f'Accuracy: {accuracy}'
+                                    f' \n Accuracy by language: '
+                                    f' \n {acc_by_lang} \n')
+
+
+def get_lang_by_id(lang_id: int) -> str:
+    return list(Config.class_dict.keys())[list(Config.class_dict.values()).index(lang_id)]
 
 
 if __name__ == '__main__':
@@ -104,9 +118,7 @@ if __name__ == '__main__':
         eval_resnet50 = EvaluateModel(ResNet50LangDetection(num_classes=len(Config.class_dict)),
                                       weights_path=f'/home/turib/lang_detection/weights/ResNet50/weights_03_08/model_{i}.pth',
                                       device='cuda:0')
-        acc, acc_by_lang = eval_resnet50.evaluate_long(test_data_path='/home/turib/test_data_long')
-        Path(f'/home/turib/lang_detection/eval/scores_{date.today()}.txt').open('a').write(f'Epoch: {i} \n'
-                                                                                           f'Accuracy: {acc}'
-                                                                                           f' \n Accuracy by language: '
-                                                                                           f' \n {acc_by_lang} \n')
+        acc, acc_lang = eval_resnet50.evaluate_long(test_data_path='/home/turib/test_data_long',
+                                                    no_samples_per_class=1000)
+        print_and_write_metrics(acc, acc_lang, f'ResNet50 epoch: {i}')
         # eval_resnet50.evaluate_short('/home/turib/val_data')
